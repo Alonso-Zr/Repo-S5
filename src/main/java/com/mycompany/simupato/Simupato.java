a04e486e5726d4caa00e8f6263c85bcab6176757
Martin Zavaleta <alonsoseguro27@gmail.com>
2026-05-09 17:51:32 -0500

    chore: iniciar proyecto Semana 5

---
+++ src/main/java/com/mycompany/simupato/Simupato.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simupato;
+
+/**
+ *
+ * @author sistemas
+ */
+public class Simupato {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
